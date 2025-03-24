57cb71a3a5e2520f409e24dcdd79c9151df33a07
xumoyun005 <xumoyddinxolmuminov858@gmail.com>
2025-03-24 17:19:30 +0500

    barcha serverlarni oladigan qildim

--- src/main/java/uz/cinerama/ServerParser.java
+++ src/main/java/uz/cinerama/ServerParser.java
@@ -6,7 +6,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import uz.cinerama.Server;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -15,13 +14,14 @@ import java.util.*;
 
 public class ServerParser {
 
-    private static final String AJAX_URL = "https://tsarvar.com/ajax/ru/servers/counter-strike-1.6?maxPlayersTo=32";
-
     public List<Server> getServers(int page, int limit) {
         List<Server> servers = new ArrayList<>();
 
         try {
-            HttpURLConnection connection = (HttpURLConnection) new URL(AJAX_URL).openConnection();
+            int sitePage = page * limit / 50; // 1 ta sayt sahifasida 50 ta server bo'lishi mumkin
+            URL url = new URL("https://tsarvar.com/ajax/ru/servers/counter-strike-1.6?maxPlayersTo=32&page=" + sitePage);
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             connection.setRequestProperty("User-Agent", "Mozilla/5.0");
             connection.setRequestProperty("Accept", "application/json");
@@ -32,17 +32,17 @@ public class ServerParser {
 
             ObjectMapper mapper = new ObjectMapper();
             JsonNode root = mapper.readTree(jsonResponse);
-
             String htmlContent = root.path("page").path("content").asText();
-            Document doc = Jsoup.parse(htmlContent);
 
+            Document doc = Jsoup.parse(htmlContent);
             Elements elements = doc.select(".serversList-item");
 
-            int start = page * limit;
+            int start = (page * limit) % 50;
             int end = Math.min(start + limit, elements.size());
 
             for (int i = start; i < end; i++) {
                 Element el = elements.get(i);
+
                 String name = Objects.requireNonNull(el.selectFirst(".serversList-itemName a")).text();
                 String ip = Objects.requireNonNull(el.selectFirst(".serversList-itemAddressTblCText")).text();
                 String players = Objects.requireNonNull(el.selectFirst(".serversList-itemPlayers")).text();
@@ -58,4 +58,5 @@ public class ServerParser {
 
         return servers;
     }
+
 }
